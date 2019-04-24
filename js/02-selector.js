a63c6169ef0e6afa4414f850b99ff55e22d24a2a
booldook <booldook@gmail.com>
2019-04-24 10:53:28 +0900

    modulus

--- js/02-selector.js
+++ js/02-selector.js
@@ -1,12 +1,12 @@
 // ES5
 var x = 10;
 x += 5;
-console.log(x);	//15
+//console.log(x);	//15
 
 var t = '<div class="mt">';
 t += '<p>'+x+'</p>';
 t += '</div>';
-console.log(t);	//<div class="mt">15</div>
+//console.log(t);	//<div class="mt">15</div>
 
 // Java
 // int a = "10"; //Error
@@ -33,12 +33,30 @@ Man.name = "홍길순";
 
 
 for(var i=0; i<10; i++) {
-	console.log(i);
+	//console.log(i);
 }
-console.log("마지막 값: " + i);
+//console.log("마지막 값: " + i);
 
 let j = 0;
 for(let j=0; j<10; j++) {
-	console.log(j);
+	//console.log(j);
 }
-console.log("마지막 값: " + j);
+//console.log("마지막 값: " + j);
+
+var x = 10 ** 10;
+console.log(x);
+
+// %의 사용법
+
+var prdArr = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,7,8];
+var html = '';
+html = '<tr>';
+for(var i=0; i<prdArr.length; i++) {
+	html += '<td>'+i+'</td>';
+	if(i % 3 == 2) {
+		html += '</tr>';
+		html += '<tr>';
+	}
+}
+html += '</tr>';
+document.querySelector("#prds>tbody").innerHTML = html;
