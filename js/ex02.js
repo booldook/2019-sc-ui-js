1b70634697a99317c91f8ae53f77176807a2c40f
booldook <booldook@gmail.com>
2019-04-26 17:29:53 +0900

    ex02

--- js/ex02.js
+++ js/ex02.js
@@ -1,3 +1,4 @@
+/*
 var wid = 100;
 var hei = 100;
 var bg = "blue";
@@ -34,4 +35,45 @@ $("#bt_reset").click(function(){
 		"border": "none",
 		"transform": "translate(0px, 0px) rotate(0deg)"
 	});
+});
+*/
+
+var wid = 100;
+var hei = 100;
+var bg = "blue";
+var borWid = 0;
+var borColor = "";
+var borStyle = "";
+var trX = 0;
+var trY = 0;
+var deg = 0;
+var d = document;
+var f = document.forms;
+var box = d.querySelector(".box");
+d.querySelector("#bt_save").addEventListener("click", function(){
+	if(f.wid.value != "") wid = f.wid.value;
+	if(f.hei.value != "") hei = f.hei.value;
+	if(f.bg.value != "") bg = f.bg.value;
+	if(f.bor_wid.value != "") borWid = f.bor_wid.value;
+	if(f.bor_color.value != "") borColor = f.bor_color.value;
+	if(f.bor_style.value != "") borStyle = f.bor_style.value;
+	if(f.tr_x.value != "") trX = f.tr_x.value;
+	if(f.tr_y.value != "") trY = f.tr_y.value;
+	if(f.deg.value != "") deg = f.deg.value;
+	box.style.width = wid+"px";
+	box.style.height = hei+"px";
+	box.style.backgroundColor = bg;
+	box.style.border = borWid + "px " + borStyle + " " + borColor;
+	box.style.WebkitTransform = "translate("+trX+"px, "+trY+"px) rotate("+deg+"deg)";	//safari
+	box.style.msTransform = "translate("+trX+"px, "+trY+"px) rotate("+deg+"deg)";			//ie9
+	box.style.transform = "translate("+trX+"px, "+trY+"px) rotate("+deg+"deg)";				//ie10 ~ chrome
+});
+d.querySelector("#bt_reset").addEventListener("click", function(){
+	box.style.width = "100px";
+	box.style.height = "100px";
+	box.style.backgroundColor = "blue";
+	box.style.border = "none";
+	box.style.WebkitTransform = "translate(0px, 0px) rotate(0deg)";
+	box.style.msTransform = "translate(0px, 0px) rotate(0deg)";
+	box.style.transform = "translate(0px, 0px) rotate(0deg)";
 });
