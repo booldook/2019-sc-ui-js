1277741572128b7c8acf89919cc29e46d6df64ac
booldook <booldook@gmail.com>
2019-04-26 11:28:29 +0900

    05-object

--- js/05-object.js
+++ js/05-object.js
@@ -1,7 +1,4 @@
 // 계산기
-var a = 0;
-var b = 0;
-var c = 0;
 function add(x, y) {
 	return x + y;
 }
@@ -17,3 +14,28 @@ function div(x, y) {
 function mod(x, y) {
 	return x % y;
 }
+/*
+$("#bt_solve").click(function(){
+	$("#res").val(eval($("#a").val() + $("#oper").val() + $("#b").val()));
+});
+*/
+$("#bt_solve").click(function(){
+	var a = $("#a").val();
+	var b = $("#b").val();
+	var oper = $("#oper").val();
+	var result = eval(a + oper + b);	// "10" + "*" + "20" => "10*20"	=> eval("10*20")	=> 200
+	$("#res").val(result);
+});
+/*
+document.getElementById("bt_solve").addEventListener("click", function(){
+	var a = document.getElementById("a").value;
+	var b = document.getElementById("b").value;
+	var oper = document.getElementById("oper").value;
+	var result = eval(a + oper + b);
+	document.getElementById("res").value = result;
+});
+*/
+
+//var res = eval("10" + "+" + "20");
+//console.log(res);
+
