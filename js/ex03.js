1d7addefda1e919a6a55686eb88345ba736af215
booldook <booldook@gmail.com>
2019-04-29 13:41:16 +0900

    js animation

--- js/ex03.js
+++ js/ex03.js
@@ -14,26 +14,35 @@ $("#bt_reset").click(function(){
 });
 */
 
+var jumbo = document.querySelector(".jumbotron");
+var jumboCSS = getComputedStyle(jumbo, null);
+var str = "abcdef";
+var str2 = str.replace("cd", "ab");
+//console.log(str);
+//console.log(str2);
 function gen(str) {
 	return Number(str.replace("px", ""));
 }
+
 var stage = document.querySelector(".stage");
 var stageCSS = getComputedStyle(stage);
 var box = document.querySelector(".box");
 var boxCSS = getComputedStyle(box);
-var tar = gen(stageCSS.width) - gen(boxCSS.width);
-var gap = 8.58;
-var pos = 0;
-var interval = null;
-function ani() {
-	pos = pos + gap;
-	if(	gen(box.style.left) >= tar ) {
-		box.style.left = tar + "px";
-		clearInterval(interval);
-		//callback 실행위치
-	}
-	else box.style.left = pos + "px";
-}
+var fps = 60;				// 초당 60장의 frame으로 Animation 구현
+var sec = 2;				// Animation 진행 초
+var ms = 1000/fps;	//	16.67 - setInterval 간격
+var tar = 0;				// 움직일 Animation 총 거리
+var step = 0;				// 한번에 움직일 거리
+var pos = 0;				// 이동후 현재 위치
+window.addEventListener("resize", function(){
+	tar = gen(stageCSS.width) - gen(boxCSS.width);
+	step = tar/(fps*sec);	// frame 한번당 움직일 거리(px)
+	console.log(tar, step);
+});
+window.dispatchEvent(new Event("resize"));
+
+
+
 document.querySelector("#bt_move").addEventListener("click", function(){
 	clearInterval(interval);
 	interval = setInterval(ani, 16.67);
