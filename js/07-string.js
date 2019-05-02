8b75e00986460237e8c6b151e7e0c5c43005027f
booldook <booldook@gmail.com>
2019-05-02 11:18:38 +0900

    table 생성

--- js/07-string.js
+++ js/07-string.js
@@ -10,3 +10,60 @@ btReset.addEventListener("click", function(){
 	dvConsole.innerHTML = '';
 });
 
+// 문자열 만들기
+var html = '<button onclick="alert(\'hi\');"></button>';		// OK
+var html2 = "<button onclick=\"alert('hi');\"></button>";		// OK
+
+// 문자열 안에서 줄바꿈
+//alert("아버지를 아버지라 부르지 못하고\n형을 형이라 부르지 못하고..."); 
+
+// 문자열과 변수가 혼합된 긴 문자열 처리
+var score = [];
+score[0] = {name: "홍길동", kor: 95, eng: 90, math: 85};
+score[1] = {name: "홍길순", kor: 90, eng: 95, math: 88};
+score[2] = {name: "홍길이", kor: 95, eng: 90, math: 85};
+score[3] = {name: "홍길만", kor: 85, eng: 85, math: 95};
+
+function makeTable() {
+	$("#console").empty();
+	// table 생성
+	var $table = $('<table></table>').appendTo($("#console"));
+	$table.addClass("table table-bordered table-hover text-center");
+	
+	// thead 생성
+	var $thead = $('<thead></thead>').appendTo($table);
+	$thead.append('<tr></tr>');
+	$thead.children('tr').append('<td>이름</td>'); 
+	$thead.children('tr').append('<td>국어</td>'); 
+	$thead.children('tr').append('<td>영어</td>'); 
+	$thead.children('tr').append('<td>수학</td>'); 
+	$thead.css("background-color", "#ccc");
+
+	// tbody 생성
+	var $tbody = $('<tbody></tbody>').appendTo($table);
+	for(var i=0 in score) {
+		$tbody.append('<tr><td>'+score[i].name+'</td><td>'+score[i].kor+'</td><td>'+score[i].eng+'</td><td>'+score[i].math+'</td></tr>');
+	}
+}
+
+$("#bt_table").click(makeTable);
+
+
+/*
+	<thead>
+		<tr>
+			<td>이름</td>
+			<td>국어</td>
+			<td>영어</td>
+			<td>수학</td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>홍길동</td>
+			<td>75점</td>
+			<td>85점</td>
+			<td>95점</td>
+		</tr>
+	</tbody>
+*/
